97a83643cbed536296e908c044a669df07cb8d18
Lucas <lucas.twisk@gmail.com>
2014-04-08 18:48:47 +0200

    Restructured javascript

--- koclicker.js
+++ koclicker.js
@@ -1,35 +1,65 @@
-var str = 0;
-var def = 0;
-var spd = 0;
-var agl = 0;
+//Used variables
+var str;
+var def;
+var spd;
+var agl;
+var loc;
 
-$("#str").click(function () {
-    str++;
-    $("#strmeter").text(str);
-});
-
-$("#def").click(function () {
-    def++;
-    $("#defmeter").text(def);
-});
-
-$("#spd").click(function () {
-    spd++;
-    $("#spdmeter").text(spd);
-});
-
-$("#agl").click(function () {
-    agl++;
-    $("#aglmeter").text(agl);
-});
 $(document).ready(function(){
-$("#weaponsO").click(function () {
-    $("#glove").slideUp();
-    $("#weapons").slideToggle();
+	//TODO: If cookie does not exists
+	newGame();
 });
 
-$("#gloveO").click(function () {
-    $("#weapons").slideUp();
-    $("#glove").slideToggle();
-});
-});
+function newGame() {
+	initVariables();
+	playGame();
+
+}
+
+function initVariables() {
+	str = 0;
+	def = 0;
+	spd = 0;
+	agl = 0;
+	loc = "gym"
+}
+
+function playGame() {
+	setListeners();
+}
+
+function setListeners() {
+	// Gym-specific listeners
+	$("#str").click(function () {
+	    str++;
+	    $("#strmeter").text(str);
+	});
+	
+	$("#def").click(function () {
+	    def++;
+	    $("#defmeter").text(def);
+	});
+	
+	$("#spd").click(function () {
+	    spd++;
+	    $("#spdmeter").text(spd);
+	});
+	
+	$("#agl").click(function () {
+	    agl++;
+	    $("#aglmeter").text(agl);
+	});
+	
+	// Shop-specific listeners
+	$("#weaponsO").click(function () {
+	    $("#glove").slideUp();
+	    $("#weapons").slideToggle();
+	});
+	
+	$("#gloveO").click(function () {
+	    $("#weapons").slideUp();
+	    $("#glove").slideToggle();
+	});
+}
+	
+
