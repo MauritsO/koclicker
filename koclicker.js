03d099da3e6592e4d02f533213cd07c35f8e79d4
Lucas <lucas.twisk@gmail.com>
2014-04-10 12:27:10 +0200

    start on battle stuffa

--- koclicker.js
+++ koclicker.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-var str = 0;
-var def = 0;
-var spd = 0;
-var agl = 0;
-var usern = "";
-var henk = {health:10, strength:str, defence:def, speed:spd, agility:agl};
-var enemy1 = {health:10, strength:3};
-var enemy2 = {health:20, strength:15};
-var enemy3 = {health:30, strength:100};
-var enemy = [enemy1, enemy2, enemy3]; //list of enemies
-
-
-function add(){
-    $("#str").click(function () {
-    str++;
-    $("#strmeter").text(str);
-});
-}
-
-n=0; // 0 is the first enemy in the list.
-$("#bat").click(function () {
-    alert("You have gone to battle");
-    if(str > enemy[n].strength && enemy[n].strength < henk.health)
-        {alert("gj you won")
-    n++; // if the enemy is beaten it will go on to the next enemy in the list
-}
-    else
-        {alert("Lost, nub. The enemy has strength " + enemy[n].strength + " and you only have strength " + str)}
-});
-
-=======
 //Used variables
 var str;
 var def;
@@ -39,7 +7,7 @@ var items;
 var loc;
 var usern;
 var inv;
->>>>>>> FETCH_HEAD
+var health;
 
 //shop-related
 var gloves;
@@ -74,10 +42,12 @@ function initVariables() {
 	def = 0;
 	spd = 0;
 	agl = 0;
+	health = 10;
 	shop = [];
 	inv = [];
 	usern = askUsername();
 	loc = "gym"
+	createShopItems();
 }
 
 function playGame() {
@@ -97,12 +67,13 @@ function askUsername() {
 	}
 }
 
+// Render shop
 function setShop() {
-	createItems();
-	setItems();
+	updateItems();
 }
 
-function createItems() {
+// Init shop items on new game
+function createShopItems() {
 	shop = [];
 	shop.push(new item("Gloves", 1,  150, 100, 20, 5));
 	shop.push(new item("Shield", 1,  0, 300, 0, 0));
@@ -110,7 +81,8 @@ function createItems() {
 	shop.push(new item("Potion", 1, 10, 10, 10, 10));
 }
 
-function setItems() {
+// Update items in the shop list
+function updateItems() {
 	shopDiv = $("#items");
 	var shopCode = "";
 	for (var i = 0; i < shop.length; i++) {
@@ -119,18 +91,18 @@ function setItems() {
 	shopDiv.html(shopCode)
 }
 
+// Buy item from shop
 function buyItem(elem) {
 	var id = elem.id;
 	inv[id] = shop[id];
-	console.log(inv);
-	
+
 	shop[id].lvl++;
 	shop[id].str = Math.round(shop[id].str * 1.1);
 	shop[id].def = Math.round(shop[id].def * 1.1);
 	shop[id].spd = Math.round(shop[id].spd * 1.1);
 	shop[id].agl = Math.round(shop[id].agl * 1.1);
 	
-	setItems();
+	updateItems();
 	updateStats();
 }
 
@@ -139,6 +111,7 @@ function updateStats() {
 	var tot_def = +def;
 	var tot_spd = +spd;
 	var tot_agl = +agl;
+	var tot_health = +health;
 	
 	for (var i = 0; i < inv.length; i++) {
 		if (typeof inv[i] !== "undefined") {
@@ -148,11 +121,17 @@ function updateStats() {
 			tot_agl += inv[i].agl;
 		}
 	}
+	
+	$("#strmeter").text(str);
+	$("#defmeter").text(def);
+	$("#spdmeter").text(spd);
+	$("#aglmeter").text(agl);
 
  	$("#totalstr").text("Strength: " + Math.round(tot_str));
  	$("#totaldef").text("Defense: " + Math.round(tot_def));
  	$("#totalspd").text("Speed: " + Math.round(tot_spd));
  	$("#totalagl").text("Agility: " + Math.round(tot_agl));
+  	$("#totalhealth").text("Health: " + Math.round(tot_health));
 }
 
 
@@ -194,14 +173,89 @@ function setListeners() {
 	});
 	
 	// Non-specific listeners
-	$("#save").click(function(){
-	    setCookie("str",str,365);
-	    setCookie("def",def,365);
-	    setCookie("spd",spd,365);
-	    setCookie("agl",agl,365);
+	$("#save").click(function () {
+	    setCookie("str", str, 365);
+	    setCookie("def", def, 365);
+	    setCookie("spd", spd, 365);
+	    setCookie("agl", agl, 365);
+	    setCookie("health", health, 365);
+	    setCookie("shop", JSON.stringify(shop), 365);
+	    setCookie("inv", JSON.stringify(inv), 365);
+	});
+	
+	$("#bat").click(function () {
+		playBattle();
 	});
 }
 
+// Load player (when cookie present) variables
+function loadVariables(){
+    str = getCookie("str");
+    def = getCookie("def");
+    spd = getCookie("spd");
+    agl = getCookie("agl");
+    health = getCookie("health");
+    shop = getCookie("shop");
+    inv = getCookie("inv");
+    
+    if(str === "")
+        str = 0;
+    if(def === "")
+        def = 0;
+    if(spd === "")
+        spd = 0;
+    if(agl === "")
+        agl = 0;
+    if(health === "")
+        health = 10;
+    if (shop === "") {
+    	shop = [];
+    } else {
+    	shop = JSON.parse(shop);
+    }
+    
+    if (inv === "") {
+    	inv = [];
+    } else {
+    	inv = JSON.parse(inv);
+    }
+}
+
+// Constructor for item
+function item(name, lvl, str, def, spd, agl) {
+	this.lvl = lvl;
+	this.name = name;
+	this.str = str;
+	this.def = def;
+	this.spd = spd;
+	this.agl = agl;
+}
+
+// Match related stuff:
+//-------------------------
+var enemy1 = {health:10, strength:3};
+var enemy2 = {health:20, strength:15};
+var enemy3 = {health:30, strength:100};
+var enemy = [enemy1, enemy2, enemy3]; //list of enemies
+
+var n = 0; // 0 is the first enemy in the list.
+
+function playBattle() {
+	console.log("Arrived at battle");
+    alert("You have gone to battle");
+    if (str > enemy[n].strength && enemy[n].strength < health) {
+    	alert("gj you won");
+    n++; // if the enemy is beaten it will go on to the next enemy in the list
+	} else {
+		alert("Lost, nub. The enemy has strength " + enemy[n].strength + " and you only have strength " + str);
+	}
+}
+
+	
+
+// Cookie related stuff:
+//-------------------------
+// Check if cookie in browser cache
 function isCookie() {
 	var user=getCookie("username");
 	if (user!=="") {
@@ -212,6 +266,7 @@ function isCookie() {
 	}
 }
 
+// Save value to cookie
 function setCookie(cname,cvalue,exdays) {
 	var d = new Date();
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
@@ -219,6 +274,7 @@ function setCookie(cname,cvalue,exdays) {
 	document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
+// get value from cookie
 function getCookie(cname) {
 	var name = cname + "=";
 	var ca = document.cookie.split(';');
@@ -229,138 +285,5 @@ function getCookie(cname) {
 	return "";
 }
 
-function loadVariables(){
-    str = getCookie("str");
-    def = getCookie("def");
-    spd = getCookie("spd");
-    agl = getCookie("agl");
-    if(str ==="")
-        str = 0;
-    if(def ==="")
-        def = 0;
-    if(spd ==="")
-        spd = 0;
-    if(agl ==="")
-        agl = 0;
-    $("#strmeter").text(str);
-    $("#defmeter").text(def);
-    $("#spdmeter").text(spd);
-    $("#aglmeter").text(agl);
-<<<<<<< HEAD
-});
 
-$("#weaponsO").click(function () {
-    $("#glove").slideUp();
-    $("#weapons").slideToggle();
-});
 
-$("#gloveO").click(function () {
-    $("#weapons").slideUp();
-    $("#glove").slideToggle();
-});
-
-$(".shop").hover(function () {
-    $(this).css("background-color", "#eeeeee");
-},
-
-function () {
-    $(this).css("background-color", "#e5eecc");
-});
-
-$(document).ready(function () {
-    $("#wl").append("<div>" + "Shit" + "</div>");
-    checkCookie();
-    $("#user").text(usern);
-    load();
-});
-
-$("#tt").blur(function(){
-    $("#inp").text($(this).val());
-});
-
-$("#save").click(function(){
-    setCookie("str",str,365);
-    setCookie("def",def,365);
-    setCookie("spd",spd,365);
-    setCookie("agl",agl,365);
-});
-
-$("#load").click(function(){
-    load();
-});
-
-$("#event").click(function(){
-    var d=new Date();
-    var t=d.toLocaleTimeString();
-    $("<div />").text("EVENT! " + t).prependTo("#log");
-});
-
-function setCookie(cname,cvalue,exdays)
-{
-var d = new Date();
-d.setTime(d.getTime()+(exdays*24*60*60*1000));
-var expires = "expires="+d.toGMTString();
-document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname)
-{
-var name = cname + "=";
-var ca = document.cookie.split(';');
-for(var i=0; i<ca.length; i++)
-  {
-  var c = ca[i].trim();
-  if (c.indexOf(name)===0) return c.substring(name.length,c.length);
-}
-return "";
-}
-
-function checkCookie()
-{
-var user=getCookie("username");
-if (user!=="")
-  {
-  usern = user;
-  }
-else
-  {
-  user = prompt("Please enter your name:","");
-  if (user!=="" && user!==null)
-    {
-    setCookie("username",user,365);
-    }
-  }
-}
-
-function load(){
-    str = getCookie("str");
-    def = getCookie("def");
-    spd = getCookie("spd");
-    agl = getCookie("agl");
-    if(str ==="")
-        str = 0;
-    if(def ==="")
-        def = 0;
-    if(spd ==="")
-        spd = 0;
-    if(agl ==="")
-        agl = 0;
-    $("#strmeter").text(str);
-    $("#defmeter").text(def);
-    $("#spdmeter").text(spd);
-    $("#aglmeter").text(agl);
-}
-=======
-}
-
-function item(name, lvl, str, def, spd, agl) {
-	this.lvl = lvl;
-	this.name = name;
-	this.str = str;
-	this.def = def;
-	this.spd = spd;
-	this.agl = agl;
-}
-	
-
->>>>>>> FETCH_HEAD
