0101f17ad8a5500a96bffc3bafa0fbe7ae67024e
Lucas <lucas.twisk@gmail.com>
2014-04-10 10:29:32 +0200

    Shoppp

--- koclicker.js
+++ koclicker.js
@@ -6,6 +6,7 @@ var agl;
 var items;
 var loc;
 var usern;
+var inv;
 
 //shop-related
 var gloves;
@@ -16,15 +17,13 @@ var shop;
 
 
 $(document).ready(function(){
+	inv = []
 	if (isCookie()) {
-		console.log("cookie present");
 		loadGame();
 	} else {
-		console.log("no cookie");
 		newGame();
 	}
 	var hoi = shop[1].name;
-	console.log(hoi);
 });
 
 function newGame() {
@@ -42,9 +41,7 @@ function initVariables() {
 	def = 0;
 	spd = 0;
 	agl = 0;
-
 	shop = [];
-
 	inv = [];
 	usern = askUsername();
 	loc = "gym"
@@ -52,7 +49,9 @@ function initVariables() {
 
 function playGame() {
 	setListeners();
+	updateStats();
 	setShop();
+	
 }
 
 function askUsername() {
@@ -67,45 +66,87 @@ function askUsername() {
 
 function setShop() {
 	createItems();
-	//if (loc == "gym") {
-	//	room.prepend('<img id="gym" src="gym.png" />');
-	//}
+	setItems();
 }
 
 function createItems() {
 	shop = [];
-	shop.push(new item("Gloves", 150, 100, 20, 5));
-	shop.push(new item("Shield", 0, 300, 0, 0));
-	shop.push(new item("Shoes", 10, 0, 250, 250));
-	shop.push(new item("Potion", 10, 10, 10, 10));
+	shop.push(new item("Gloves", 1,  150, 100, 20, 5));
+	shop.push(new item("Shield", 1,  0, 300, 0, 0));
+	shop.push(new item("Shoes", 1, 10, 0, 250, 250));
+	shop.push(new item("Potion", 1, 10, 10, 10, 10));
 }
 
 function setItems() {
 	shopDiv = $("#items");
-
-	shopDiv.html('')
+	var shopCode = "";
+	for (var i = 0; i < shop.length; i++) {
+		shopCode += '<li id = "' + i + '"onclick="buyItem(this)">' + shop[i].name + '(level: ' + shop[i].lvl + ')</li>';
+	}
+	shopDiv.html(shopCode)
 }
+
+function buyItem(elem) {
+	var id = elem.id;
+	inv[id] = shop[id];
+	console.log(inv);
+	
+	shop[id].lvl++;
+	shop[id].str = Math.round(shop[id].str * 1.1);
+	shop[id].def = Math.round(shop[id].def * 1.1);
+	shop[id].spd = Math.round(shop[id].spd * 1.1);
+	shop[id].agl = Math.round(shop[id].agl * 1.1);
+	
+	setItems();
+	updateStats();
+}
+
+function updateStats() {
+	var tot_str = +str;
+	var tot_def = +def;
+	var tot_spd = +spd;
+	var tot_agl = +agl;
+	
+	for (var i = 0; i < inv.length; i++) {
+		if (typeof inv[i] !== "undefined") {
+			tot_str += inv[i].str;
+			tot_def += inv[i].def;
+			tot_spd += inv[i].spd;
+			tot_agl += inv[i].agl;
+		}
+	}
+
+ 	$("#totalstr").text("Strength: " + Math.round(tot_str));
+ 	$("#totaldef").text("Defense: " + Math.round(tot_def));
+ 	$("#totalspd").text("Speed: " + Math.round(tot_spd));
+ 	$("#totalagl").text("Agility: " + Math.round(tot_agl));
+}
+
 
 function setListeners() {
 	// Gym-specific listeners
 	$("#str").click(function () {
 	    str++;
 	    $("#strmeter").text(str);
+	    updateStats();
 	});
 	
 	$("#def").click(function () {
 	    def++;
 	    $("#defmeter").text(def);
+	    updateStats();
 	});
 	
 	$("#spd").click(function () {
 	    spd++;
 	    $("#spdmeter").text(spd);
+	    updateStats();
 	});
 	
 	$("#agl").click(function () {
 	    agl++;
 	    $("#aglmeter").text(agl);
+	    updateStats();
 	});
 	
 	// Shop-specific listeners
@@ -174,7 +215,8 @@ function loadVariables(){
     $("#aglmeter").text(agl);
 }
 
-function item(name, str, def, spd, agl) {
+function item(name, lvl, str, def, spd, agl) {
+	this.lvl = lvl;
 	this.name = name;
 	this.str = str;
 	this.def = def;
